f60947a74c5b01b6cc4bdb7fe006e1ba750c76b8
squidsk <squidsk@gmail.com>
2022-12-19 10:45:31 -0700

    Starting to create the interface

---
+++ ATVTW/ATVTWWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TWLibrary;
+
+
+namespace ATVTW
+{
+    /// <summary>
+    /// Interaction logic for ATVTWWindow.xaml
+    /// </summary>
+    public partial class ATVTWWindow : Window
+    {
+        private Settings settings;
+        private readonly HardcodedLists hcLists;
+        private readonly CustomLists cLists;
+        private readonly Options options;
+
+        public ATVTWWindow() {
+            options = new Options();
+        }
+
+        private void CommandCanExecute(object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = true;
+        }
+
+        private void CommandNeedsSettingsToExecute(object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = settings != null;
+        }
+
+        private void OpenSetting(object sender, RoutedEventArgs e) {
+            MessageBox.Show("Open Setting is not yet implemented!", "Open Setting", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        private void NewSetting(object sender, RoutedEventArgs e) {
+            MessageBox.Show("New Setting is not yet implemented!", "New Setting", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        private void ExitProgram(object sender, ExecutedRoutedEventArgs e) {
+            Close();
+        }
+
+        private void StopForWarnings(object sender, ExecutedRoutedEventArgs e) {
+            //options.StopForWarnings = OptionsWarningsMenuItem.IsChecked;
+        }
+
+        private void About(object sender, RoutedEventArgs e) {
+            ATVTWAbout aboutWindow = new ATVTWAbout(this);
+            aboutWindow.ShowDialog();
+        }
+
+
+    }
+}
---
+++ ATVTW/TWCommands.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+
+namespace ATVTW
+{
+    static class TWCommands {
+        public static RoutedUICommand CreateSetting { get; private set; }
+        public static RoutedUICommand About { get; private set; }
+        public static RoutedUICommand Validate { get; private set; }
+        public static RoutedUICommand StopForWarnings { get; private set; }
+        public static RoutedUICommand AutoIndent { get; private set; }
+
+        static TWCommands() {
+            CreateSetting = new RoutedUICommand("Create Setting", "Create Setting", typeof(TWCommands));
+            CreateSetting.InputGestures.Add(new KeyGesture(Key.C, ModifierKeys.Control | ModifierKeys.Alt));
+            About = new RoutedUICommand("About", "About", typeof(TWCommands));
+            Validate = new RoutedUICommand("Validate", "Validate", typeof(TWCommands));
+            Validate.InputGestures.Add(new KeyGesture(Key.V, ModifierKeys.Alt));
+            StopForWarnings = new RoutedUICommand("StopForWarnings", "StopForWarnings", typeof(TWCommands));
+            AutoIndent = new RoutedUICommand("AutoIndent", "AutoIndent", typeof(TWCommands));
+        }
+    }
+}
